44c2d09a3bb54b52bd8655bb7c0a12b6c8464785
Kevin Tallevi <ktallevi@ehealthinnovation.org>
2017-11-24 15:10:51 -0500

    Initial Commit

---
+++ ContinuousGlucoseOnFhir/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ContinuousGlucoseOnFhir
+//
+//  Created by Kevin Tallevi on 11/24/17.
+//  Copyright © 2017 eHealth Innovation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
